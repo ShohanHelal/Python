ff260a5044e8e16ad04c8b952cd709d20298ffee
ShohanHelal <shohanhelal725@gmail.com>
2026-01-24 13:15:46 +0600

    Stat lession

---
+++ Lession/Condition.py
@@ -0,0 +1,9 @@
+a = int(input("Enter The Value of A : "))
+b = int(input("Enter The Value of B : "))
+c = int(input("Enter The Value of C : "))
+if a>b & a>c :
+    print("A is Bigger")
+elif b>a & b>c:
+    print("B is Bigger") 
+else :
+    print("C is Bigger")   
