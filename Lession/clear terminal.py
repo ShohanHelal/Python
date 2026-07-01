25c120e2f82f365f14a651b6f85787c0ae02b3c9
ShohanHelal <shohanhelal725@gmail.com>
2026-07-01 18:11:32 +0600

    clear Terminal added

---
+++ Lession/clear terminal.py
@@ -0,0 +1,7 @@
+import os
+while True : 
+    os.system('cls') # this is the clear for window. it work in this loop
+    a = input("Enter Your Name : ")
+    print(a)
+    input("Press Enter to Continue!!!")# if we don't type this . the system will clear without printing anything
+    #for both window and linux 'os.system('cls' if os.neme == 'nt' else 'clear')'
