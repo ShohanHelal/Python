f15b4a644c58d5f3ae220db16ad0de8494dc8b34
ShohanHelal <shohanhelal725@gmail.com>
2026-07-02 15:19:23 +0600

    dictionary added

---
+++ Lession/Dictionary.py
@@ -0,0 +1,19 @@
+info = {
+    "name" : "Helal",
+    "Roll" : 121,
+    "age"  : 22,
+    "friends" : ["Rafi","arpon","sabbir"],
+    "subjects" : {
+        "phy" : 87,
+        "chem": 86,
+        "mth" : 88
+    }
+}
+
+print("name")
+print(len(info))
+info["name"] = "Shohan" #rewrite
+print(info)
+null_dict = {}
+null_dict["Game"] = "GTA V"
+
