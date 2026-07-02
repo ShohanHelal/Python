55fa7540e0706cb5cb00982a5f169e9b7caaa9b7
ShohanHelal <shohanhelal725@gmail.com>
2026-07-02 15:20:13 +0600

    some practice

---
+++ Lession/FirstProgram.py
@@ -0,0 +1,12 @@
+name = "Helal"
+roll = 121
+print("Hellow World.\nGo home")
+print("my name is",name,"\nMy Roll is",roll)
+print(type(name))
+print(type(roll))
+de = 23.5
+print(roll+de)
+print(roll,de)
+tre = ["helal", "shohan" , "noyon"]
+for i in range(len(tre)):
+    print(tre[i] , end = " ") #end used to change (\n , ,"")
