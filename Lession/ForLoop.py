02b15637a9be4a1dde1350b06d70db46a8d7d4fa
ShohanHelal <shohanhelal725@gmail.com>
2026-07-03 22:47:13 +0600

    For loop Added

---
+++ Lession/ForLoop.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print(i)
+#Loop with Range (Start , End ) End number won't be Print.
+for j in range(20,30):
+    print(j)     
+#Loop with Steps
+for k in range(1,10,2):
+    print(k)    
