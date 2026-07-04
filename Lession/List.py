bc189e194c0f19948b9d4d0324accfd64ee20dad
ShohanHelal <shohanhelal725@gmail.com>
2026-07-05 01:40:48 +0600

    list added

---
+++ Lession/List.py
@@ -0,0 +1,15 @@
+a = [1,2,3,4]
+print(a)
+sum = a[1] + a[3]
+print(sum)
+#slicing
+print(a[1:3]) #[start : end] end indx not will be included
+#sort mathod
+a.sort() #this mathod directly changes this don't return anything. it's Like void
+print(a)
+a.sort(reverse = True)
+print(a)
+print(len(a)) #size of the List
+#a quick brown fox running over the lazy dog 
+
+    
