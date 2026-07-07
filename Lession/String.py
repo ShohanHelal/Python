b71bc72ec4bba28b0cd5613192872e9ff5a29e2a
ShohanHelal <shohanhelal725@gmail.com>
2026-07-07 18:19:37 +0600

    String added

---
+++ Lession/String.py
@@ -0,0 +1,9 @@
+str1 = "Shohan"
+str2 = "Helal"
+final_Str = str1 + str2 #adding 2 String
+length = len(final_Str) #for the length of a String
+print(length , final_Str)
+print(final_Str[0]) #it will print the char at 0 position in the string 
+#Slicing
+str3 = final_Str[6:12]#[start : end] end will notbe printed
+print(str3)
