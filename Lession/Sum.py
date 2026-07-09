92060ccbe8abe2c4680468126350d11f919a19c3
ShohanHelal <shohanhelal725@gmail.com>
2026-07-09 20:14:36 +0600

    Sum and Tuple

---
+++ Lession/Sum.py
@@ -0,0 +1,6 @@
+a = 2
+b = 3 
+sum = a+b
+diff = b - a
+print("The Sum of A  & B =",sum)
+print("The Different between A & B =",diff)
---
+++ Lession/Tuples.py
@@ -0,0 +1,6 @@
+#tuples can't be changed
+t = (4,2,1,4) # tuples use parenthesis
+print(t)
+#mathods :
+print(t[1:3])
+
