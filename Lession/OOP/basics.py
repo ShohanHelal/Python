bea6167f0d933d82deddd1d8426b609f1bad8c49
ShohanHelal <shohanhelal725@gmail.com>
2026-07-19 01:21:55 +0600

    New OOP

---
+++ Lession/OOP/basics.py
@@ -0,0 +1,8 @@
+class student : #class creating
+    name = "helal"
+    roll = "121"
+
+s1 = student() #object creating / s1 is refarence
+print(s1.name) 
+#refarance_name.mathod/attributs_name
+print(s1.roll)
